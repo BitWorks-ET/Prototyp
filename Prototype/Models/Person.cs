591bd399d33e3c8ff56510a17d52003a16193d9d
cmerkens <cmerkens@spirit21.com>
2025-04-05 21:46:31 +0200

    Login System aufgebaut

--- Prototype/Models/Person.cs
+++ Prototype/Models/Person.cs
@@ -2,15 +2,14 @@
 
 public class Person
 {
-    // Properties
     public string FirstName { get; set; }
-    public string LastName { get; set; } 
-    public Gender Gender { get; set; }
-    
-    public string Email { get; set; }
+    public string LastName { get; set; }
+    public DateTime? Birthday { get; set; }
+    public Gender? Gender { get; set; }
     public string PhoneNumber { get; set; }
     public string JobTitle { get; set; }
 
     public int Id { get; set; }
-    public string Password { get; set; }
+    public string eMail { get; set; }
+    public string PasswordHash { get; set; }
 }
